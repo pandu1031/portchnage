89a42dbae0da43e8db1eda6a999077e75d56d141
pandu1031 <130541596+pandu1031@users.noreply.github.com>
2023-06-06 15:21:59 +0530

    Update tomcatport.groovy

--- tomcatport.groovy
+++ tomcatport.groovy
@@ -7,7 +7,7 @@ pipeline{
     stages{
         stage("change port number"){
             steps{
-                sh """ ssh -i /tmp/mamu1031.pem ec2-user@${SERVERIP} "sudo sed -zi 's/8080/9080/2' /etc/tomcat/server.xml"
+                sh """ ssh -i /tmp/mamu1031.pem ec2-user@${SERVERIP} "sudo sed -zi 's/8080/9090/2' /etc/tomcat/server.xml"
                 ssh -i /tmp/mamu1031.pem ec2-user@${SERVERIP} "sudo systemctl restart tomcat"
                  """
             }
